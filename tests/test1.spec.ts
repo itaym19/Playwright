6f0851932ac1658dc28191a3cd8e0b3fb72dde59
Itay Mantsur <Itaym@team.co.il>
2026-06-29 18:27:02 +0300

    second commit

--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from "@playwright/test";
 
 type field = { name: string; type: string };
-
+const url = "https://test.netlify.app/";
+//function that returns array of page input fields
 async function discoverElements(page: any, url: string): Promise<field[]> {
   await page.goto(url);
   const formFields: field[] = await page
@@ -16,5 +17,18 @@ async function discoverElements(page: any, url: string): Promise<field[]> {
   return formFields;
 }
 test("list elements", async ({ page }) => {
-  await discoverElements(page, "https://test.netlify.app/");
+  const fields: field[] = await discoverElements(page, url);
+  await page.goto(url);
+  for (let i: number = 0; i < fields.length; i++) {
+    if (fields[i].type === "select-one") {
+      const options: string[] = await page
+        .locator(`select[name ="${fields[i].name}"] option`)
+        .evaluateAll((select: any) => {
+          return select.map((option: any) => {
+            return option.value;
+          });
+        });
+      console.log(options);
+    }
+  }
 });
