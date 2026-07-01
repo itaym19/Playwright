7da406db0ea5e90f5120863ccb356b2670b88054
Itay Mantsur <Itaym@team.co.il>
2026-07-01 18:56:20 +0300

    7th commit

--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -1,51 +1,61 @@
 import { test, expect } from "@playwright/test";
-import {field, discoverElements, isValidEmail, isValidUrl} from '../modules'
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
+import { field, discoverElements, isValidEmail, isValidUrl } from "../modules";
+import * as readline from "node:readline";
+import { stdin as input, stdout as output } from "node:process";
+
 //function to use rl.question in an sync way
 function askQuestion(prompt: string): Promise<string> {
-    return new Promise((resolve) => {
-        rl.question(prompt, resolve);
-    })
+  return new Promise((resolve) => {
+    rl.question(prompt, resolve);
+  });
 }
 
 const url = "https://test.netlify.app/";
 
 const rl = readline.createInterface({ input, output });
 
-test("list elements", async ({ page }) => { 
+test("list elements", async ({ page }) => {
   await page.goto(url);
-  const fields: field[] = await discoverElements (page,"form input, form select, form textarea, form button",);
+  const fields: field[] = await discoverElements(page, "form input, form select, form textarea, form button");
   //Go through each field, tell the user the field type and recieve a value to fill in the field
-  for (const instance: field of fields) {
-    let userInput: string = '';
+  for (const instance of fields) {
+    let userInput: string = "";
     switch (instance.type) {
-      case 'text':
-        async () => {
-          userInput = await askQuestion(`Please enter your ${instance.name}: `)
+      case "text":
+        userInput = await askQuestion(`Please enter your ${instance.name}: `);
+        break;
+
+      case "email":
+        while (!isValidEmail(userInput)) {
+          userInput = await askQuestion(`Please enter your ${instance.name}: `);
         }
         break;
 
-      case 'email':
-        async () => {
-          while (!isValidEmail(userInput)) {
-            userInput = await askQuestion(`Please enter your ${instance.name}: `)
-          }
+      case "url":
+        while (!isValidUrl(userInput)) {
+          userInput = await askQuestion(`Please enter your ${instance.name}: `);
         }
         break;
-      
-      case 'url':
-        async () => {
-          while (!isValidUrl(userInput)) {
-            userInput = await askQuestion(`Please enter your ${instance.name}: `)
-          }
+
+      case "select-one":
+        const options: string[] = await page.locator(`select[name = ${instance.name}]`).evaluateAll((options) => options.map((option: any) => option.value));
+        while (!(userInput in options)) {
+          userInput = await askQuestion(`select number of employees: ${options.join(", ")}`);
         }
+        break;
+
+      case "submit":
+        page.locator('button[type="submit"]').click();
+        console.log(`form was submitted`);
+        break;
+
+      default:
+        console.log("not a valid input type");
         break;
     }
   }
-})
+});
 
-  
 /*  for (let i: number = 0; i < fields.length; i++) {
     if (fields[i].type === "select-one") {
       const options: string[] = await page
@@ -58,4 +68,3 @@ test("list elements", async ({ page }) => {
       console.log(options);
     } 
   } */
-
